941d6577b89d5b7af54e41ac25f465fdf3bcfdda
黎健聪 <2496778408@qq.com>
2022-01-20 12:36:52 +0800

    变更

--- spring_mvc/src/main/java/com/controller/UserController.java
+++ spring_mvc/src/main/java/com/controller/UserController.java
@@ -2,10 +2,17 @@ package com.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
-    @RequestMapping("/quick")
+    @RequestMapping(value = "/quick1")
+    public String save1(){
+        System.out.println("UserController running...");
+        return "success.jsp";
+    }
+
+    @RequestMapping(value = "/quick")//method = RequestMethod.GET,params = {"username"}
     public String save(){
         System.out.println("UserController running...");
         return "success.jsp";
